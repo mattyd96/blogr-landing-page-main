44708cc6f352acc60c267d8b0b24e4f83e03fbf2
Matthew <furikuribeebop@gmail.com>
2021-06-16 23:39:33 +0930

    desktop layout finished. need to redo mobile layout

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+//helper function to toggle hidden status by class name
 function toggleNested(parent) {
-    console.log("clicked");
     var element = document.getElementsByClassName(parent)[0].classList.toggle("hidden");
-    console.log(element);
 }
 
+//Get all sub-item nav lists, show clicked element, hide others
+function toggleSub(parent, icon) {
+    var elements = document.getElementsByClassName("sub-items");
+    Array.from(elements).forEach(element => {
+
+        if(!element.classList.contains("hidden") 
+            && !element.classList.contains(parent)) {
+            element.classList.toggle("hidden");
+        }
+    });
+    toggleNested(parent);
+    document.getElementsByClassName(icon)[0].classList.toggle("up");
+}
+
+//toggle hamburger icon for mobile menu
 function hamburgerToggle(parent) {
     toggleNested(parent);
     var element = document.getElementsByClassName("hamburger")[0].src;
-    console.log(element);
 
     if(element.endsWith('icon-hamburger.svg')) {
         document.getElementsByClassName("hamburger")[0].src='images/icon-close.svg';
     } else {
         document.getElementsByClassName("hamburger")[0].src='images/icon-hamburger.svg';
     }
-}
-
-function toggleSub(parent, icon) {
-    toggleNested(parent);
-    console.log(document.getElementsByClassName(icon)[0])
-    document.getElementsByClassName(icon)[0].classList.toggle("up");
 }
